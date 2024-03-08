aa83485739061a0cbe36c2e2c7b42f506a1b11ef
AlfathRamadhan <alfathramadhan@gmail.com>
2024-03-08 11:47:25 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+ Selamat datang di contact
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KatalogController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+ // Route::get('/', function ()) {
+ //     return view('welcome');
+ // });
+ 
+Route::get('/', [KatalogController::class, 'index']);
+Route::get('/contact', [KatalogController::class, 'contact']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
